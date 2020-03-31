ed152ae1f5162cb97813d6ec6a1d10fb75ab163e
San-Alienware <abwing@abwing.com>
2020-03-31 17:03:29 +0700

    KU SuperBirds

---
+++ Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayManager : MonoBehaviour
+{
+    public GameObject originalPipe;
+    public float pipeSpacing = 3f;
+    public float gapRange = 1f;
+    public GameObject canvasGameOver;
+    public Text textScore;
+    public Text textBestScore;
+    public int score = 0;
+    public int best = 0;
+    public bool gameOver = false;
+
+    void Start()
+    {
+        GenerateLevel();
+        if (PlayerPrefs.HasKey("BEST"))
+        {
+            best = PlayerPrefs.GetInt("BEST");
+        }
+    }
+    void Update()
+    {
+        
+    }
+    public void GenerateLevel()
+    {
+        for(int i=0; i<10; i++)
+        {
+            GameObject pipe = Instantiate(originalPipe);
+            pipe.transform.position = new Vector3(i * pipeSpacing, Random.Range(-gapRange, gapRange), 0);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        canvasGameOver.SetActive(true);
+        textBestScore.text = PlayerPrefs.GetInt("BEST").ToString();
+    }
+
+    public void AddScore()
+    {
+        if (gameOver)
+            return;
+
+        score++;
+        textScore.text = score.ToString();
+
+        if(score > best)
+        {
+            PlayerPrefs.SetInt("BEST", score);
+            PlayerPrefs.Save();
+        }
+        
+    }
+
+    public void ResetBestScore()
+    {
+        PlayerPrefs.SetInt("BEST", 0);
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/SuperBird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperBird : MonoBehaviour
+{
+    Rigidbody rb;
+    public float jumpPower = 3f;
+    public float forwardSpeed = 2f;
+    GamePlayManager gm;
+    public float maxSpeed = 3f;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        gm = FindObjectOfType<GamePlayManager>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.right * forwardSpeed);
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        gm.GameOver();
+        rb.isKinematic = true;
+    }
+}
